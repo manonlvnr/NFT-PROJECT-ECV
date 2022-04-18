7f8c9b6bd46e29182cb7f9d055e9606aaf297f76
Manon <manonmacbookpro@MacBook-Pro-de-Manon.local>
2022-04-18 12:20:50 +0200

    Add navbar to access favs

---
+++ Favs.js
@@ -0,0 +1,15 @@
+function addFavs() {
+    
+}
+
+function displayFavs() {
+    nftContainer.innerHTML = "";
+    
+    if (localStorage.length === 0) {
+        // const divContainer = document.createElement('div');
+        // divContainer.innerHTML = "Oh no... you don't have any favorites"
+    } else {
+        console.log(localStorage);
+    }
+
+}
