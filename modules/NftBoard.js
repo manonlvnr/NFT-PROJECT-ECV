551d531f27bdf5d9a30b546a25277ddeb1e977f1
louis cobi <louis.cobigo@gmail.com>
2022-04-21 01:20:19 +0200

    Last Rush B cyka (part1)

--- modules/NftBoard.js
+++ modules/NftBoard.js
@@ -1,15 +1,10 @@
 const root = document.querySelector("body");
 
 function createElement(tag, config, parent = null) {
-  const { nameNFT, src, ownerNFT, creatorNFT, descriptionNFT, idNFT } = config || {};
-
+  const { nameNFT, src, idNFT } = config || {};
   const element = document.createElement(tag);
-  // if (color) {
-  //     element.style.color = color;
-  // }
+  
 
-  // if (text) {
-  //     element.innerHTML = text;
   if (config) {
     // Créer un élément img avec l'url de l'image et le mettre dans la variable myDiv
     let cardElement = document.createElement("div");
@@ -17,22 +12,16 @@ function createElement(tag, config, parent = null) {
     let imageElement = document.createElement("img");
     let infoContainer = document.createElement("div");
     let name = document.createElement("h5");
-    let description = document.createElement("p");
-    let creator = document.createElement("p");
-    let owner = document.createElement("p");
     let buyBtn = document.createElement("button");
     let likeBtn = document.createElement("button");
     //let idCard = idNFT
     //nftCard.setAttribute('id', idNFT);
-    cardElement.className = "flex font-sans p-8 m-8 card nftImage";
+    cardElement.className = "flex font-sans card nftImage m-8 w-[40%] transition duration-300 ease-in-out hover:scale-105 hover:drop-shadow-2xl";
     cardElement.setAttribute('onclick',"runFavs(this)");
     imageContainer.className = "flex-none w-56 relative shadow-md";
     imageElement.className = "absolute inset-0 w-full h-full object-cover rounded-lg ";
-    infoContainer.className = "flex-auto p-6 shadow-md";
+    infoContainer.className = "flex-auto p-6 shadow-md bg-white";
     name.className = "w-full flex-none mt-2 order-1 text-3xl font-bold text-violet-600";
-    description.className = "text-sm text-slate-500 ";
-    creator.className = "flex-auto font-medium text-slate-900";
-    owner.className = "text-sm font-medium text-slate-400";
     buyBtn.className = "h-10 px-6 font-semibold rounded-full bg-violet-600 text-white";
     likeBtn.className = "flex-none flex items-center justify-center w-9 h-9 rounded-full text-violet-600 bg-violet-50";
     
@@ -43,78 +32,20 @@ function createElement(tag, config, parent = null) {
     if (nameNFT) {
       name.innerText = nameNFT;
     }
-    if (descriptionNFT) {
-      description.innerText = descriptionNFT;
-    }
-    if (creatorNFT) {
-      creator.innerText = creatorNFT;
-    }
-    if (ownerNFT) {
-      owner.innerText = ownerNFT;
-    }
     if (idNFT){
       cardElement.setAttribute('id', idNFT);
     }
-    buyBtn.innerText = "Buy it";
+    buyBtn.innerText = "View more";
+    buyBtn.onclick = function () {
+        window.location.href = `single.html?id=${idNFT}`;
+    }
 
-    cardContainer.appendChild(cardElement);
+    nftContainer.appendChild(cardElement);
     cardElement.append(imageContainer, infoContainer);
 
     imageContainer.appendChild(imageElement);
-    infoContainer.append(name, description, creator, owner, buyBtn, likeBtn);
-
-    const tiltEffectSettings = {
-        max: 25, // max tilt rotation (degrees (deg))
-        perspective: 1000, // transform perspective, the lower the more extreme the tilt gets (pixels (px))
-        scale: 1.1, // transform scale - 2 = 200%, 1.5 = 150%, etc..
-        speed: 500, // speed (transition-duration) of the enter/exit transition (milliseconds (ms))
-        easing: "cubic-bezier(.03,.98,.52,.99)" // easing (transition-timing-function) of the enter/exit transition
-      };
+    infoContainer.append(name, buyBtn, likeBtn);
     
-    const cards = document.querySelectorAll(".card ");
-      
-    cards.forEach(card => {
-      card.addEventListener("mouseenter", cardMouseEnter);
-      card.addEventListener("mousemove", cardMouseMove);
-      card.addEventListener("mouseleave", cardMouseLeave);
-    });
-    
-    function cardMouseEnter(event) {
-      setTransition(event);
-    }
-    
-    function cardMouseMove(event) {
-      const card = event.currentTarget;
-      const cardWidth = card.offsetWidth;
-      const cardHeight = card.offsetHeight;
-      const centerX = card.offsetLeft + cardWidth/2;
-      const centerY = card.offsetTop + cardHeight/2;
-      const mouseX = event.clientX - centerX;
-      const mouseY = event.clientY - centerY;
-      const rotateXUncapped = (+1)*tiltEffectSettings.max*mouseY/(cardHeight/2);
-      const rotateYUncapped = (-1)*tiltEffectSettings.max*mouseX/(cardWidth/2);
-      const rotateX = rotateXUncapped < -tiltEffectSettings.max ? -tiltEffectSettings.max : 
-                      (rotateXUncapped > tiltEffectSettings.max ? tiltEffectSettings.max : rotateXUncapped);
-      const rotateY = rotateYUncapped < -tiltEffectSettings.max ? -tiltEffectSettings.max : 
-                      (rotateYUncapped > tiltEffectSettings.max ? tiltEffectSettings.max : rotateYUncapped);
-    
-      card.style.transform = `perspective(${tiltEffectSettings.perspective}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) 
-                              scale3d(${tiltEffectSettings.scale}, ${tiltEffectSettings.scale}, ${tiltEffectSettings.scale})`;
-    }
-    
-    function cardMouseLeave(event) {
-      event.currentTarget.style.transform = `perspective(${tiltEffectSettings.perspective}px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)`;
-      setTransition(event);
-    }
-    
-    function setTransition(event) {
-      const card = event.currentTarget;
-      clearTimeout(card.transitionTimeoutId);
-      card.style.transition = `transform ${tiltEffectSettings.speed}ms ${tiltEffectSettings.easing}`;
-      card.transitionTimeoutId = setTimeout(() => {
-        card.style.transition = "";
-      }, tiltEffectSettings.speed);
-    }
   }
   if (!parent) {
     root.appendChild(element);
@@ -125,10 +56,8 @@ function createElement(tag, config, parent = null) {
 }
 
 const nftContainer = createElement("div");
-nftContainer.className = "nft-container flex";
-const cardContainer = createElement("div");
-cardContainer.className = "flex-1";
-nftContainer.appendChild(cardContainer);
+nftContainer.className = "nft-container flex flex-wrap justify-center mt-9";
+nftContainer.appendChild(cardElement);
 // Créateur
 // Nombre de ventes
 // Détails des produits NFT
@@ -146,7 +75,7 @@ function createCards(assets) {
         descriptionNFT: el.description,
         idNFT: el.id,
       },
-      cardContainer
+      nftContainer
     );
   });
 }
@@ -157,8 +86,9 @@ async function createIndex() {
     const response = await fetch(`https://awesome-nft-app.herokuapp.com/`);
     const data = await response.json();
     const assets = data.assets;
-
+    if(assets){
     createCards(assets);
+}
   } catch (error) {
     console.log(error);
   }
