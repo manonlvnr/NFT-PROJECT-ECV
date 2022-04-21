7d3ca6153da431d096e779e4e0d158f865582a3b
Manon Lavenier <manonmacbookpro@MacBook-Pro-de-Manon.local>
2022-04-21 10:35:15 +0200

    Rearrange all

--- modules/NftBoard.js
+++ modules/NftBoard.js
@@ -1,7 +1,7 @@
 const root = document.querySelector("body");
 
 function createElement(tag, config, parent = null) {
-  const { nameNFT, src, idNFT } = config || {};
+  const { nameNFT, src, ownerNFT, creatorNFT, descriptionNFT, idNFT } = config || {};
   const element = document.createElement(tag);
   
 
@@ -12,18 +12,31 @@ function createElement(tag, config, parent = null) {
     let imageElement = document.createElement("img");
     let infoContainer = document.createElement("div");
     let name = document.createElement("h5");
+    // let description = document.createElement("p");
+    // let creator = document.createElement("p");
+    // let owner = document.createElement("p");
     let buyBtn = document.createElement("button");
     let likeBtn = document.createElement("button");
+    let heartBtn = document.createElement("img");
+    likeBtn.appendChild(heartBtn);
     //let idCard = idNFT
     //nftCard.setAttribute('id', idNFT);
     cardElement.className = "flex font-sans card nftImage m-8 w-[40%] transition duration-300 ease-in-out hover:scale-105 hover:drop-shadow-2xl";
-    cardElement.setAttribute('onclick',"runFavs(this)");
+    // cardElement.setAttribute('onclick',"runFavs(this)");
     imageContainer.className = "flex-none w-56 relative shadow-md";
     imageElement.className = "absolute inset-0 w-full h-full object-cover rounded-lg ";
-    infoContainer.className = "flex-auto p-6 shadow-md bg-white";
+    infoContainer.className = "flex-auto p-6 shadow-md";
     name.className = "w-full flex-none mt-2 order-1 text-3xl font-bold text-violet-600";
+    // description.className = "text-sm text-slate-500 ";
+    // creator.className = "flex-auto font-medium text-slate-900";
+    // owner.className = "text-sm font-medium text-slate-400";
     buyBtn.className = "h-10 px-6 font-semibold rounded-full bg-violet-600 text-white";
-    likeBtn.className = "flex-none flex items-center justify-center w-9 h-9 rounded-full text-violet-600 bg-violet-50";
+    likeBtn.className = "flex-none flex items-center justify-center w-10 h-10 rounded-full likeBtn";
+    likeBtn.setAttribute('onclick',"runFavs(this)");    
+    likeBtn.style.backgroundColor = "grey";
+    heartBtn.className = "w-7 h-7"
+    heartBtn.src = "https://emojitool.com/img/facebook/4.0/white-heart-1566.png";
+    
     
     if (src) {
       imageElement.src = src;
@@ -32,6 +45,7 @@ function createElement(tag, config, parent = null) {
     if (nameNFT) {
       name.innerText = nameNFT;
     }
+   
     if (idNFT){
       cardElement.setAttribute('id', idNFT);
     }
